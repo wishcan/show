a4a47a5a9c55c303bde00d288cff0f873ecf9ed8
祝亚辉 <410345759@qq.com>
2013-01-09 11:20:12 +0800

    -a

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -7,6 +7,7 @@ public function actionIndex()
 	{
 		// renders the view file 'protected/views/site/index.php'
 		// using the default layout 'protected/views/layouts/main.php'
+		echo md5('admin');
 		$this->render('index');
 	}
 
