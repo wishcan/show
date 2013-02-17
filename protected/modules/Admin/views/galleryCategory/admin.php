0e281f3dfda2600e707e78777b6ca785b09b2f2d
unknown <410345759@qq.com>
2013-02-17 23:22:40 +0800

    第二次提交

--- protected/modules/Admin/views/galleryCategory/admin.php
+++ protected/modules/Admin/views/galleryCategory/admin.php
@@ -7,6 +7,6 @@ $this->breadcrumbs=array(
 <a href='<?php echo Yii::app()->createAbsoluteurl('Admin/galleryCategory/create');?>' class='btn' />
 创建分类
 </a>
-<a href='<?php echo Yii::app()->createAbsoluteurl('Admin/gallery/create');?>' class='btn' />
+<a href='<?php echo Yii::app()->createAbsoluteurl('Admin/gallery/create');?>' class='btn' target='blank' />
 上传图片
 </a>
